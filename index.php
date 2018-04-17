d5ac7784dea867ef68641abf92ec17288a4ff24f
amyeverett1 <amy.everett@digital.homeoffice.gov.uk>
2018-04-17 19:38:10 +0100

    stuff

--- index.php
+++ index.php
@@ -12,15 +12,16 @@
          <h3><i class="fa fa-info-circle fa-lg" aria-hidden="true"></i><span>Event Information</span></h3>
     <div>
         <div class="wrapper">
-            <p>The Digital, Data and Technology roundtables are informal face to face sessions with the DDaT senior management team.</p>
+            <p>Lorem ipsum dolor sit amet, eos et ferri solum detracto, an vivendo omittam detracto nam. Accusamus assueverit vel et. Ut cum quot nulla delicatissimi, saepe iudico comprehensam no mel. Ut cum graece eruditi, falli molestiae te est, at mel omnes ponderum invenire. Ridens fierent quo in.</p>
 
-<p>They are an opportunity for relaxed ‘get to know you and your work’ conversations with colleagues and senior managers who you might not usually meet in your day to day activities and to help build wider awareness, understanding and collaboration across DDaT. We may also theme some of the conversations to address key or topical subjects.</p>
+<p>No enim audire persequeris mei. Error intellegat id mea, vel diam ludus an. Ex per sumo tota omittam, id quo case legendos. Te wisi aperiam eleifend cum, id ius veri contentiones, nam ea probo tantas facilisis. At vel alii ferri, duo dicit epicuri ad.</p>
 
-<p>Sessions will be co-hosted by senior managers and take place across our various locations on a quarterly basis.</p>
+<p>Sea putent invenire delicatissimi no, ea eam mundi vocibus iracundia. Munere consequuntur reprehendunt has ei, dicat assum usu te. At cum sonet legere ocurreret, eu tantas intellegam eum. Choro facilisi pri in, nonumy voluptua ei mea. Errem perfecto sit an, error scaevola sententiae quo ne.</p>
 
-<p>All sessions are open to everyone across DDaT (subject to business commitments) and <strong>places must be booked through this app</strong>. Tickets are available on a first come first served basis and waiting lists will be in operation. You are not expected to travel specifically to attend a session; further roundtables will be held at each site on a rolling basis so there will be plenty of opportunities to attend throughout the year. Roundtables will not be used as a channel to announce any major updates – if required, such information would be shared in the usual ways with all staff. </p>
+<h3>Ex dicant laboramus usu</h3>
+<p>debet simul gloriatur no eum. Vero ocurreret ius ne. His ut case ridens complectitur. Tota nominati gloriatur ad his, facer iudicabit interesset ei vis, alii solet persequeris ea est. Ludus labitur facilisi te vis. Eum in dicunt feugiat.</p>
 
-<p>Once you have booked a place this will be considered as your commitment to attend. Calendar appointments and reminders will be sent out before the date but <strong>please keep revisiting this app for the latest updates</strong> or changes to the agenda.</p>
+<p>Commune intellegam definitionem quo ei, nam soluta viderer molestie at, te denique molestiae reformidans qui. Hinc docendi temporibus id nam, ei nibh nominavi dissentiunt ius. Vix in iudico laudem detracto. Et eros choro fuisset ius. Et nec graeci splendide neglegentur, elitr vocent no pri.</p>
         </div><!--wrapper-->
     </div><!-- end venue-->
 
@@ -38,7 +39,7 @@
          <i class="fa fa-globe fa-lg" aria-hidden="true"></i> <span>Location</span></h3>
      <div>
         <div class="wrapper">
-          
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2483.98415913436!2d-0.13179834912096572!3d51.495158179533114!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487604dd3047d689%3A0x71700a3a19e9e83d!2s2+Marsham+St%2C+Westminster%2C+London+SW1P+4DF!5e0!3m2!1sen!2suk!4v1523989956276" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
             </div>
         </div><!--wrapper-->
         </div><!-- end hendon-->
@@ -68,6 +69,11 @@
 
         <footer>
             <p>For the best experience using this site, we recommend you use Google Chrome or the Microsoft Edge browser.</p>
+
+            <div class="logo_box">
+              <img src="/img/logo.png" alt="">
+              <img src="/img/m.png" alt="">
+            </div>
     </footer>
 
 </div>
