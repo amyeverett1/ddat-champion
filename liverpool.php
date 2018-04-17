5aa23883d001df4d928325db230f7a447689799a
amyeverett1 <amy.everett@digital.homeoffice.gov.uk>
2018-04-17 19:05:37 +0100

    all files

---
+++ liverpool.php
@@ -0,0 +1,64 @@
+<?php include('header.php') ?>
+    <h1>Liverpool Roundtable</h1>
+</header>
+
+<div class="date">
+    <span>2nd floor training room, 101 Old Hall Street <br> 8 March 2018 <br> 10:00am - 11:00am </span>
+</div>
+
+<div class="container index"> 
+<div class="accordion">  
+    
+    <h3><i class="fa fa-list-ol fa-lg" aria-hidden="true"></i> <span>Agenda</span></h3>
+    <div>
+        <div class="wrapper">
+            <p>The Digital, Data and Technology roundtables are informal face to face sessions with the DDaT senior management team. They are an opportunity for relaxed ‘get to know you and your work’ conversations with colleagues and senior managers who you might not usually meet in your day to day activities and to help build wider awareness, understanding and collaboration across DDaT.</p>
+
+            <p>All sessions are open to everyone across DDaT (subject to business commitments). You do not need to book in advance; places will be available on a first come first served basis on the day. You are not expected to travel specifically to attend a session; further roundtables will be held at each site on a rolling basis so there will be plenty of opportunities to attend throughout the year.</p>
+
+            <p>There is no fixed agenda or formal presentation and you can raise any topic you wish at these events. This is your roundtable to find out more about the wider work of DDaT and discuss developments, local issues and concerns. It is also an opportunity to share information about your work, what your team is doing and your successes.  You can also raise questions and issues in advance by emailing the DDaT Communications team.  Alternatively, if you’d prefer to submit questions and/or flag issues anonymously, please <a href="http://www.homeofficesurveys.homeoffice.gov.uk/s/RoundtableQuestions/">submit questions or queries using this link</a>.</p>
+
+            <p>These events are coordinated by <a href="mailto:HODDaTcommunications@homeoffice.gsi.gov.uk">DDaT Communications</a>.</p>
+            <p>We won't be releasing tickets for this event. If you wish to come along please just turn up.</p>
+            
+            <p><strong>10:00am Start</strong></p>     
+            <ul>
+            <li>Welcome and open – Simon Black</li>
+            <li>Introductions – All</li>
+            <li>Business updates and key developments – Simon Black</li>
+            <li>Hendon updates including successes – All</li>
+            <li>Questions and discussion topics submitted in advance by attendees – All</li>
+                </ul>
+            <p><strong>11:00am Close</strong></p>
+                <p>Please contact <a href="mailto:HODDaTcommunications@homeoffice.gsi.gov.uk">HO DDaT Communications</a> if you have any queries.</p>    
+        </div> <!-- wrapper-->
+    </div><!-- end agenda-->
+
+
+    
+    <h3><i class="fa fa-map-marker fa-lg" aria-hidden="true"></i> <span>Venue</span></h3>
+    <div>
+        <div class="wrapper">
+            <div class="box white">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2378.2531363062562!2d-2.9983314845964473!3d53.41029887999248!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487b2133197774f9%3A0x2382195b5332f6ba!2s101+Old+Hall+St%2C+Liverpool+L3+9BP!5e0!3m2!1sen!2suk!4v1491241129297" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div><!--boxwhite--> 
+        </div><!--wrapper--> 
+    
+        <div class="wrapper">
+            <p>Don’t forget to bring your Home Office pass. You must show it when you arrive. If you don’t bring it, you may not be able to gain access to the building.</p>
+            <h3>Address</h3>
+            <p>2nd floor training room<br> 101 Old Hall Street<br> 2nd Floor<br>IT Operations<br> Liverpool<br>L3 9BP</p>
+            </div><!--wrapper--> 
+        </div><!-- end venue-->
+        
+<h3><i class="fa fa-info-circle fa-lg" aria-hidden="true"></i>  <span>Submit your questions</span></h3>
+    <div>
+        <div class="wrapper">
+            <p>There is no fixed agenda or formal presentation and you can raise any topic you wish at these events.</p>
+            <p>We are keen for attendees to raise questions, issues and suggested discussion topics in advance by emailing the DDaT Communications team.  Alternatively <a href="http://www.homeofficesurveys.homeoffice.gov.uk/s/RoundtableQuestions/">submit questions and/or flag issues anonymously using this link</a>.</p>
+        </div><!--wrapper-->
+  </div><!-- end questions-->
+    
+</div> <!--end accordion--> 
+
+<?php include('footer.php')?>
---
+++ southport.php
@@ -0,0 +1,68 @@
+<?php include('header.php') ?>
+    <h1>Southport Roundtable</h1>
+</header>
+
+<div class="date">
+    <span>Block F, Smedley Hydro, Southport, PR8 2HH  <br> 8 March 2018 <br> 1:30PM - 2:30pm </span>
+</div>
+
+<div class="container index"> 
+<div class="accordion">    
+    
+    <h3><i class="fa fa-list-ol fa-lg" aria-hidden="true"></i> <span>Agenda</span></h3>
+    <div>
+        <div class="wrapper">
+            <p>The Digital, Data and Technology roundtables are informal face to face sessions with the DDaT senior management team. They are an opportunity for relaxed ‘get to know you and your work’ conversations with colleagues and senior managers who you might not usually meet in your day to day activities and to help build wider awareness, understanding and collaboration across DDaT.</p>
+
+            <p>All sessions are open to everyone across DDaT (subject to business commitments). You do not need to book in advance; places will be available on a first come first served basis on the day. You are not expected to travel specifically to attend a session; further roundtables will be held at each site on a rolling basis so there will be plenty of opportunities to attend throughout the year.</p>
+
+            <p>There is no fixed agenda or formal presentation and you can raise any topic you wish at these events. This is your roundtable to find out more about the wider work of DDaT and discuss developments, local issues and concerns. It is also an opportunity to share information about your work, what your team is doing and your successes.  You can also raise questions and issues in advance by emailing the DDaT Communications team.  Alternatively, if you’d prefer to submit questions and/or flag issues anonymously, please <a href="http://www.homeofficesurveys.homeoffice.gov.uk/s/RoundtableQuestions/">submit questions or queries using this link</a>.</p>
+
+            <p>These events are coordinated by <a href="mailto:HODDaTcommunications@homeoffice.gsi.gov.uk">DDaT Communications</a>.</p>
+              <p>We won't be releasing tickets for this event. If you wish to come along please just turn up.</p>
+            <p><strong>13:30pm Start</strong></p>     
+            <ul>
+                <li>Welcome and open – Simon Black</li>
+                <li>Introductions – All</li>
+                <li>Business updates and key developments – Simon Black</li>
+                <li>Hendon updates including successes – All</li>
+                <li>Questions and discussion topics submitted in advance by attendees – All</li>
+             </ul>
+            <p><strong>14:30pm Close</strong></p>
+            <p>Please contact <a href="mailto:HODDaTcommunications@homeoffice.gsi.gov.uk">HO DDaT Communications</a> if you have any queries.</p>    
+        </div> <!-- wrapper-->
+    </div><!-- end agenda-->
+    
+    <h3><i class="fa fa-map-marker fa-lg" aria-hidden="true"></i> <span>Venue</span></h3>
+    <div>
+        <div class="wrapper">    
+            <div class="box white">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2365.9196271425203!2d-3.022398384150822!3d53.63057283003967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487b3eb4e454b105%3A0xd48b94a46481c85e!2sSmedley+Hydro%2C+Southport!5e0!3m2!1sen!2suk!4v1491052471010" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div><!--boxwhite--> 
+        </div><!--wrapper--> 
+
+        <div class="wrapper">
+            <p>Don’t forget to bring your Home Office pass. You must show it when you arrive. If you don’t bring it, you may not be able to gain access to the building.</p>
+            <h3>Address</h3>
+            <p>Block F<br>
+                Smedley Hydro<br>
+                Trafalgar Road<br>
+                Southport<br>
+                PR8 2HH</p>   
+            <p>The nearest train station is Birkdale, which is approximately an 8 minute walk to the venue.</p>
+        </div><!--wrapper--> 
+    </div><!-- end venue-->
+
+    <h3><i class="fa fa-info-circle fa-lg" aria-hidden="true"></i>  <span>Submit your questions</span></h3>
+    <div>
+        <div class="wrapper">
+            <p>There is no fixed agenda or formal presentation and you can raise any topic you wish at these events.</p>
+            <p>We are keen for attendees to raise questions, issues and suggested discussion topics in advance by emailing the DDaT Communications team.  Alternatively <a href="http://www.homeofficesurveys.homeoffice.gov.uk/s/RoundtableQuestions/">submit questions and/or flag issues anonymously using this link</a>.</p>
+        </div><!--wrapper--> 
+    </div><!-- end questions-->
+
+
+</div> <!--end accordion--> 
+            
+
+<?php include('footer.php')?>
